297494c10dbf957cc8dbebe6eb56d352603daf1f
Alistair Francis <alistair@alistair23.me>
2024-04-02 20:19:14 +1000

    Split out I2CDevice set_address() from trait
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- boards/apollo3/lora_things_plus/src/main.rs
+++ boards/apollo3/lora_things_plus/src/main.rs
@@ -132,7 +132,10 @@ struct LoRaThingsPlus {
     console: &'static capsules_core::console::Console<'static>,
     i2c_master: &'static capsules_core::i2c_master::I2CMasterDriver<
         'static,
-        capsules_core::virtualizers::virtual_i2c::I2CDevice<'static, apollo3::iom::Iom<'static>>,
+        capsules_core::virtualizers::virtual_i2c::I2CMultiDevice<
+            'static,
+            apollo3::iom::Iom<'static>,
+        >,
     >,
     external_spi_controller: &'static capsules_core::spi_controller::Spi<
         'static,
