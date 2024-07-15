tock/tock

capsules: Add a NMEA userspace driver
Signed-off-by: Alistair Francis <alistair@alistair23.me>

capsules/core/src/driver.rs
@@ -66,6 +66,7 @@ pub enum NUM {
     SoundPressure         = 0x60006,
     AirQuality            = 0x60007,
     Pressure              = 0x60008,
+    Nmea                  = 0x60009,
 
     // Sensor ICs
     Tsl2561               = 0x70000,

UrielSaas/checkrust2

capsules/core/src/driver.rs
@@ -34,6 +34,7 @@ pub enum NUM {
     UsbUser               = 0x20005,
     I2cMasterSlave        = 0x20006,
     Can                   = 0x20007,
+    Nmea                  = 0x20008,
 
     // Radio
     BleAdvertising        = 0x30000,